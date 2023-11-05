23ec8f20087c14838e5b5de6ee77e3e70f37fc0f
xs-web-lhdd <3042146237@qq.com>
2023-11-05 15:14:01 +0800

    init project

---
+++ packages/pk1/src/index.ts
@@ -0,0 +1,5 @@
+// pkg1/src/index.ts
+
+export function pk1(): any {
+  console.log('I am pk1');
+}
---
+++ packages/pk2/src/index.ts
@@ -0,0 +1,9 @@
+// pkg2/src/index.ts
+
+import { pk1 } from '@lhdd/pk1'
+
+function pk2() {
+  pk1()
+  console.log('I am pk2');
+}
+export default pk2;
